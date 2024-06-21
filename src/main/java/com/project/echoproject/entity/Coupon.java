309dcac5588d567489a15859df9d7ce34e95b785
yoohyelin <123133622+yoohyelin@users.noreply.github.com>
2024-06-21 12:38:35 +0900

    mall java 두개

---
+++ src/main/java/com/project/echoproject/entity/Coupon.java
@@ -0,0 +1,18 @@
+package com.project.echoproject.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Coupon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long couponIdHL; // SiteUser 에 컬럼 해결
+    @Column(length = 50, nullable = false)
+    private Long couponPoint;
+    @Column(length = 50, nullable = false)
+    private String couponName;
+}
---
+++ src/main/java/com/project/echoproject/repository/CouponRepository.java
@@ -0,0 +1,8 @@
+package com.project.echoproject.repository;
+
+import com.project.echoproject.entity.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+}
