9bb0c0a36d5e12ba151fb1460cc004e1183f2c4f
youngjun-kim92 <whiteblood92@naver.com>
2024-06-21 18:39:09 +0900

    좋아요 기능 추가

--- src/main/java/com/project/echoproject/repository/LikeBoardRepository.java
+++ src/main/java/com/project/echoproject/repository/LikeBoardRepository.java
@@ -1,7 +1,10 @@
 package com.project.echoproject.repository;
 
+import com.project.echoproject.entity.AuthBoard;
 import com.project.echoproject.entity.LikeBoard;
+import com.project.echoproject.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LikeBoardRepository extends JpaRepository<LikeBoard, String> {
+public interface LikeBoardRepository extends JpaRepository<LikeBoard, Long> {
+    LikeBoard findByAuthBoardAndSiteUser(AuthBoard authBoard, SiteUser siteUser);
 }
