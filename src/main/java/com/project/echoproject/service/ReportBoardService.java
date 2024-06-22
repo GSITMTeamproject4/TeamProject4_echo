a8ef61cff7e5177c5a7bc33f2d0af98b98fdab17
youngjun-kim92 <whiteblood92@naver.com>
2024-06-23 03:01:14 +0900

    로그인폼, 회원가입폼 임시로 뒤로가기 추가

--- src/main/java/com/project/echoproject/service/ReportBoardService.java
+++ src/main/java/com/project/echoproject/service/ReportBoardService.java
@@ -1,5 +1,7 @@
 package com.project.echoproject.service;
 
+import org.springframework.ui.Model;
+
 public interface ReportBoardService {
     void reportBoard(Long listId, String userId, String reportReason, String reportContent);
 
