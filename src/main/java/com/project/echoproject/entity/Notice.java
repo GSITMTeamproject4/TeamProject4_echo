300557b4bd50d86d5e829940b3ebed0db178c815
hw <dbgpdnjs2001@naver.com>
2024-06-24 21:36:32 +0900

    hyewon_0624_공지사항 게시글 등록구현, list 전체 조회 수정 * 게시글 등록기능 초기작성, list전체조회 기능 수정 - Notice 엔티티, 레포지토리 notice_id 데이터타입 Long으로 수정 및 siteUser 외래키로 연결 - NoticeService 에 게시글 등록을 위한 create 기능 추가 - NoticeController 에 게시글 등록을 위한 createNotice 기능 추가 - NoticeFormDTO 신규생성 - notice_form.html 신규생성 - notice_list.html 에 작성자 데이터 추가

--- src/main/java/com/project/echoproject/entity/Notice.java
+++ src/main/java/com/project/echoproject/entity/Notice.java
@@ -1,11 +1,8 @@
 package com.project.echoproject.entity;
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.time.LocalDateTime;
-
 
 @Setter
 @Getter
@@ -13,8 +10,14 @@ import java.time.LocalDateTime;
 public class Notice {
 
     @Id
-    @Column(columnDefinition = "INT DEFAULT 0", nullable = true)
-    private long notice_id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long notice_id;
+
+    //siteuser의 외래키로 연결
+    //@ManyToOne: notice_list 에 작성자 데이터 보여주려면 siteuser데이터랑 notice랑 매핑
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private SiteUser siteUser;
 
     @Column(length = 50 , nullable = false)
     private String notice_title;
