86b0eeffeceaf9b63dd4b1d19aaa9c2e7f471592
youngjun-kim92 <whiteblood92@naver.com>
2024-06-25 17:19:17 +0900

    삭제시 alert 처리

--- src/main/resources/static/js/authBoard/authBoardCustom.js
+++ src/main/resources/static/js/authBoard/authBoardCustom.js
@@ -45,3 +45,11 @@ $(document).ready(function() {
         });
     });
 });
+
+function confirmDelete(form) {
+    if (confirm("정말로 삭제하시겠습니까?")) {
+        form.submit();
+    } else {
+        return false;
+    }
+}
