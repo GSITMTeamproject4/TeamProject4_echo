685209f28332f948ea02556f1219a7464d000b72
yoohyelin <123133622+yoohyelin@users.noreply.github.com>
2024-06-25 15:47:29 +0900

    챌린지 테이블 수정

--- src/main/java/com/project/echoproject/entity/Challenge.java
+++ src/main/java/com/project/echoproject/entity/Challenge.java
@@ -21,10 +21,4 @@ public class Challenge {
 
     private Date challengeDate;
 
-    @Column(length = 200, nullable = false)
-    private String checkImg;
-
-    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "imageId", referencedColumnName = "id")
-    private Image image;
 }
