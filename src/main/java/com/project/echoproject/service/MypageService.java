351ad5cc674c4d811dfdddfa09a969fd6fedf46e
yujin <jenny600437@gmail.com>
2024-06-28 10:28:29 +0900

    개인정보 수정 유효성 검사 추가, 마이 페이지 쿠폰 현황 링크 연결, 로그인 css

--- src/main/java/com/project/echoproject/service/MypageService.java
+++ src/main/java/com/project/echoproject/service/MypageService.java
@@ -1,5 +1,6 @@
 package com.project.echoproject.service;
 
+import com.project.echoproject.dto.SiteUserEditForm;
 import com.project.echoproject.entity.SiteUser;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -7,12 +8,7 @@ import java.io.IOException;
 
 public interface MypageService {
     SiteUser getUserById(String userId);
-    void updateUser(String userId, SiteUser updatedUser, MultipartFile file) throws IOException;
+    void updateUser(String userId, SiteUserEditForm updatedUser, MultipartFile file) throws IOException;
     void deleteUser(String userId);
     String encodeImageToBase64(String filePath) throws IOException;
 }
-
-
-
-
-
