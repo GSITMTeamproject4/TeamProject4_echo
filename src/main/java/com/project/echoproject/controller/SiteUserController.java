8b64a7e060c63b32956a8322ecda089304ddd210
youngjun-kim92 <whiteblood92@naver.com>
2024-06-30 18:07:45 +0900

    로그인 수정

--- src/main/java/com/project/echoproject/controller/SiteUserController.java
+++ src/main/java/com/project/echoproject/controller/SiteUserController.java
@@ -23,7 +23,7 @@ public class SiteUserController {
     // 회원가입 페이지를 반환하는 메서드입니다.
     @GetMapping("/signup")
     public String signup(SiteUserCreateForm siteUserCreateForm) {
-        return "sign_up";  // 회원가입 폼 뷰를 반환합니다.
+        return "account/sign_up";  // 회원가입 폼 뷰를 반환합니다.
     }
 
     // 회원가입 폼 제출을 처리하는 메서드입니다.
@@ -31,12 +31,12 @@ public class SiteUserController {
     public String signup(@Valid @ModelAttribute SiteUserCreateForm siteUserCreateForm,
                          BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "sign_up";
+            return "account/sign_up";
         }
         if (!siteUserCreateForm.getPassword1().equals(siteUserCreateForm.getPassword2())) {
             bindingResult.rejectValue("password2", "passwordInCorrect",
                     "2개의 패스워드가 일치하지 않습니다.");
-            return "sign_up";
+            return "account/sign_up";
         }
         try {
             siteUserService.create(siteUserCreateForm.getUserId(),
@@ -51,10 +51,10 @@ public class SiteUserController {
                     siteUserCreateForm.getDetailaddr());
         } catch (IllegalArgumentException e) {
             bindingResult.rejectValue("userId", "userIdDuplicate", e.getMessage());
-            return "sign_up";
+            return "account/sign_up";
         } catch (IOException e) {
             bindingResult.rejectValue("file", "fileUploadError", "파일 업로드 중 오류가 발생했습니다.");
-            return "sign_up";
+            return "account/sign_up";
         }
         return "index";  // 성공 시 메인 페이지로 리다이렉트
     }
@@ -63,7 +63,7 @@ public class SiteUserController {
     // 로그인 페이지를 반환하는 메서드입니다.
     @GetMapping("/login")
     public String login() {
-        return "loginForm";  // 로그인 폼 뷰를 반환합니다.
+        return "account/login";  // 로그인 폼 뷰를 반환합니다.
     }
 
     // 아이디/비밀번호 찾기 페이지를 반환하는 메서드입니다.
