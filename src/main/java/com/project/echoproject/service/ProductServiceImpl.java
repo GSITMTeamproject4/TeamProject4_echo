cba61524bc29a7dc3df74dfc9457b45040d54a5d
yoohyelin <123133622+yoohyelin@users.noreply.github.com>
2024-07-01 12:46:35 +0900

    관리자 쿠폰등록 메뉴 연결

--- src/main/java/com/project/echoproject/service/ProductServiceImpl.java
+++ src/main/java/com/project/echoproject/service/ProductServiceImpl.java
@@ -1,10 +1,14 @@
 package com.project.echoproject.service;
 
+import com.project.echoproject.entity.Coupon;
+import com.project.echoproject.entity.Image;
 import com.project.echoproject.entity.Product;
 import com.project.echoproject.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +17,7 @@ import java.util.Optional;
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
+    private final ImageService imageService;
 
     @Override
     public List<Product> getList() {
@@ -27,5 +32,20 @@ public class ProductServiceImpl implements ProductService {
         } else {
             throw new IllegalArgumentException("Product not found");
         }
+    }
+
+    public Product addItem(String name, int price, MultipartFile file) throws IOException {
+        Product product = new Product();
+
+        product.setProductName(name);
+        product.setPrice(price);
+
+        if (!file.isEmpty()) {
+            // ImageService를 사용하여 이미지 저장
+            Image image = imageService.saveImage(file);
+            product.setProductImage(image);
+            product.setCheckImg(image.getFilePath()); // checkImg 필드 설정
+        }
+        return productRepository.save(product);
     }
 }
