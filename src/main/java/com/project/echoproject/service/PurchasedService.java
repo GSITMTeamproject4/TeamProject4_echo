48ae32220b2bca5442e2e8382b8dde2e5093b5ed
bhy <bhy1999313@naver.com>
2024-07-01 09:18:30 +0900

    내구매내역

---
+++ src/main/java/com/project/echoproject/service/PurchasedService.java
@@ -0,0 +1,22 @@
+package com.project.echoproject.service;
+
+import com.project.echoproject.entity.Order;
+import com.project.echoproject.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class PurchasedService {
+
+    private final OrderRepository orderItemRepository;
+
+    @Autowired
+    public PurchasedService(OrderRepository orderRepository) {
+        this.orderItemRepository = orderRepository;
+    }
+    public List<Order> getAllOrders() {
+        return orderItemRepository.findAll();
+    }
+}
+
