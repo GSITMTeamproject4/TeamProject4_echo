e1cd490c39639ef6b61e829da2acd3b1372de798
youngjun-kim92 <whiteblood92@naver.com>
2024-07-01 17:40:52 +0900

    프로필 이미지 처리

--- src/main/java/com/project/echoproject/service/ImageService.java
+++ src/main/java/com/project/echoproject/service/ImageService.java
@@ -6,6 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 public interface ImageService {
+    Image getOrCreateDefaultImage();
     Image saveImage(MultipartFile file) throws IOException;
     String encodeImageToBase64(String filePath) throws IOException;
     void deleteImage(Image image);
